bf3d23a52ac7e44d9cb8cc77c153fd90583c09fd
PingHuskar <61967318+PingHuskar@users.noreply.github.com>
2022-04-30 01:55:18 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+import Quote from './Quote.svelte';
+let data = {};
+randomQuote()
+async function randomQuote() {
+const response = await fetch('https://api.quotable.io/random?tags=technology')
+data = await response.json()
+}
+</script>
+
+<main>
+	<h1>Random Quote App</h1>
+	<Quote {data}/>
+	<!-- <button on:click="{randomQuote}">New Quote</button> -->
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Quote.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let data = {};
+</script>
+
+
+
+<style>
+body {
+  background-color: #ecf0f1 ;
+}
+blockquote {
+  color: #2c3e50;
+  font-size: 36px;
+  line-height: 150%;
+  position: relative;
+  font-family: Georgia, serif;
+  width: 70%;
+  margin: 10% auto;
+}
+  blockquote::before {
+    content: '\201C';
+    position: absolute;
+    top: -10px;
+    left: 0;
+    font-family: Arial, sans-serif;
+    font-size: 82px;
+    font-style: normal;
+  }
+  blockquote::after {
+    content: '\201D';
+    position: absolute;
+    bottom: -60px;
+    right: 0;
+    font-family: Arial, sans-serif;
+    font-size: 82px;
+    font-style: normal;
+  }
+
+</style>
+
+<blockquote>
+    {data.content}
+</blockquote>
